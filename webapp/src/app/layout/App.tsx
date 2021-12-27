c902d7073f79c6f804a684fc28de9377755667e3
Johan Salazar <johan.itcr@gmail.com>
2021-12-27 17:07:19 -0600

    End of section 4

--- webapp/src/app/layout/App.tsx
+++ webapp/src/app/layout/App.tsx
@@ -1,36 +1,44 @@
 import Catalog from "../../features/catalog/Catalog";
-import Header from './Header';
-import { Container, createTheme, CssBaseline, Switch } from "@mui/material";
+import Header from "./Header";
+import { Container, createTheme, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
-import { useState } from 'react';
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
 
 function App() {
-
   const [darkMode, setDarkMode] = useState(false);
-  const palettleType = darkMode? 'dark': 'light';
+  const palettleType = darkMode ? "dark" : "light";
 
- const theme = createTheme({
-   palette: {
-     mode: palettleType,
-     background: {
-       default: palettleType === 'light'? '#eaeaea' : '#121212'
-     }
-   }
- })
+  const theme = createTheme({
+    palette: {
+      mode: palettleType,
+      background: {
+        default: palettleType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
 
- function handleThemeChange() {
-  setDarkMode(!darkMode);
- }
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
 
   return (
     <ThemeProvider theme={theme}>
-    <CssBaseline  />
-     <Header darkMode={darkMode} handleThemeChange={handleThemeChange}  />
- 
-     <Container>
-     <Catalog />
-     </Container>
-      
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/catalog" element={<Catalog />} />
+          <Route path="/catalog/:id" element={<ProductDetails />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+        </Routes>
+      </Container>
     </ThemeProvider>
   );
 }
--- webapp/src/features/catalog/Catalog.tsx
+++ webapp/src/features/catalog/Catalog.tsx
@@ -7,20 +7,20 @@ import ProductList from './ProductList';
 
 export default function Catalog() {
 
-    const [products, setProducts] =  useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
-    useEffect(() => {
-      
-      fetch('http://localhost:5000/api/products')
+  useEffect(() => {
+
+    fetch('http://localhost:5000/api/products')
       .then(response => response.json())
       .then(data => setProducts(data))
-    }, [])
-  
+  }, [])
 
 
-    return (
-       <>
-        <ProductList products={products} />
-       </>
-    )
+
+  return (
+    <>
+      <ProductList products={products} />
+    </>
+  )
 }
