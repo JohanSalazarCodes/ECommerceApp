5bc8e0e9bc53b33f7d15fcddb522a17c5a69ca8c
Johan Salazar <johan.itcr@gmail.com>
2021-12-28 10:06:43 -0600

    End of section 5

--- webapp/src/app/layout/App.tsx
+++ webapp/src/app/layout/App.tsx
@@ -3,11 +3,15 @@ import Header from "./Header";
 import { Container, createTheme, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ProductDetails from "../../features/catalog/ProductDetails";
 import AboutPage from "../../features/about/AboutPage";
 import ContactPage from "../../features/contact/ContactPage";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ServerError from "../../api/errors/ServerError";
+import NotFound from "../../api/errors/NotFound";
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -28,16 +32,19 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" theme="colored" hideProgressBar />
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
       <Container>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<Catalog />} />
-          <Route path="/catalog/:id" element={<ProductDetails />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/catalog" component={Catalog} />
+          <Route path="/catalog/:id" component={ProductDetails} />
+          <Route path="/about" component={AboutPage} />
+          <Route path="/contact" component={ContactPage} />
+          <Route path="/server-error" component={ServerError} />
+          <Route component={NotFound} />
+        </Switch>
       </Container>
     </ThemeProvider>
   );
--- webapp/src/features/catalog/Catalog.tsx
+++ webapp/src/features/catalog/Catalog.tsx
@@ -2,21 +2,22 @@
 import { useState, useEffect } from 'react';
 import { Product } from '../../app/models/product';
 import ProductList from './ProductList';
+import agent from '../../api/agent';
+import LoadingComponent from '../../app/layout/LoadingComponent';
 
 
 
 export default function Catalog() {
 
   const [products, setProducts] = useState<Product[]>([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    agent.Catalog.list().then(products => setProducts(products))
+    .catch(error => console.log(error))
+    .finally(() => setLoading(false))
+  }, []);
 
-    fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-  }, [])
-
-
+  if (loading) return <LoadingComponent message='Loading products...'/>
 
   return (
     <>
