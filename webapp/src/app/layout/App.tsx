66f31c447943bbac3a90d23410d82a6f999e10dd
Johan Salazar <johan.itcr@gmail.com>
2022-01-09 03:02:20 -0600

    End of section 6

--- webapp/src/app/layout/App.tsx
+++ webapp/src/app/layout/App.tsx
@@ -2,7 +2,7 @@ import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
 import { Container, createTheme, CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ProductDetails from "../../features/catalog/ProductDetails";
@@ -12,8 +12,31 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import ServerError from "../../api/errors/ServerError";
 import NotFound from "../../api/errors/NotFound";
+import BasketPage from '../../features/basket/BasketPage';
+import { useStoreContext } from "../context/StoreContext";
+import { getCookie } from '../util/util';
+import agent from '../../api/agent';
+import LoadingComponent from "./LoadingComponent";
+import CheckoutPage from '../../features/checkout/CheckoutPage';
 
 function App() {
+
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+      .then(basket => setBasket(basket))
+      .catch(error => console.log(error))
+      .finally(() => setLoading(false))
+    }
+    else{
+      setLoading(false);
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false);
   const palettleType = darkMode ? "dark" : "light";
 
@@ -30,6 +53,8 @@ function App() {
     setDarkMode(!darkMode);
   }
 
+  if (loading) return <LoadingComponent message="Initialising app..." />
+
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position="bottom-right" theme="colored" hideProgressBar />
@@ -43,6 +68,8 @@ function App() {
           <Route path="/about" component={AboutPage} />
           <Route path="/contact" component={ContactPage} />
           <Route path="/server-error" component={ServerError} />
+          <Route path="/basket" component={BasketPage} />
+          <Route path="/checkout" component={CheckoutPage} />
           <Route component={NotFound} />
         </Switch>
       </Container>
